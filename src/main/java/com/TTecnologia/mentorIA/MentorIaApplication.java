8d455b5b09b21e9397e5359cd0b3592a2f3a7d07
twedella <tamyres_wedella@hotmail.com>
2025-02-03 22:02:27 -0300

    Criar a pagina Login

---
+++ src/main/java/com/TTecnologia/mentorIA/MentorIaApplication.java
@@ -0,0 +1,13 @@
+package com.TTecnologia.mentorIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TTecnologia/mentorIA/MentorIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTecnologia.mentorIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
